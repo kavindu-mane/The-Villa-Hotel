ff250178e7006337c34afd8fbd44e245d60dbd83
DilmiSeneviratne <dilmiseneviratne@gmail.com>
2024-05-04 21:06:32 +0530

    Removed Fontawesome in Contact-us

--- src/app/(public)/(pages)/contact-us/page.tsx
+++ src/app/(public)/(pages)/contact-us/page.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
-import { faMapMarkerAlt, faPhoneAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa";
+import backgroundImage from "../../../../../public/images/img_12.jpg";
 
 const ContactUs: FC = () => {
     return (
         <>
-            <section className="contact min-h-screen p-5 md:p-20 flex justify-center items-center flex-col bg-cover" style={{ backgroundImage: `url('public/images/img_12.jpg')` }}>
+            <section className="contact min-h-screen p-5 md:p-20 flex justify-center items-center flex-col bg-cover" style={{ backgroundImage: `url(${backgroundImage})` }}>
                 <div className="content max-w-screen-md mx-auto text-center">
                     <h2 className="text-3xl font-semibold text-black">Get In Touch</h2>
                     <p className="font-light text-black">Feel free to reach out to us with any questions, comments, or inquiries you may have. We're here to help!</p>
@@ -13,21 +15,21 @@ const ContactUs: FC = () => {
                 <div className="container flex flex-col md:flex-row justify-center items-center mt-8 w-full">
                     <div className="contactInfo w-full md:w-1/2 flex flex-col">
                         <div className="box relative p-5 md:p-10 lg:p-20 flex items-center">
-                            <FontAwesomeIcon icon={faMapMarkerAlt} size="2x"  />
+                        <FaMapMarkerAlt  className="icon text-3xl"/>
                             <div className="text ml-3 md:ml-5 text-black text-base flex-col font-light">
                                 <h3 className="font-medium text-green-600">Address</h3>
-                                <p>The Villa Hotel, Yaddehimulla rd, Unawatuna, Galle</p>
+                                <p>The Villa Hotel, Yaddehimulla rd,<br/> Unawatuna,<br/> Galle</p>
                             </div>
                         </div>
                         <div className="box relative p-5 md:p-10 lg:p-20 flex items-center">
-                            <FontAwesomeIcon icon={faPhoneAlt}  size="2x"/>
+                        <FaPhoneAlt  className="icon text-3xl"/>
                             <div className="text ml-3 md:ml-5 text-black text-base flex-col font-light">
                                 <h3 className="font-medium text-green-600">Phone</h3>
                                 <p>+94 912227253</p>
                             </div>
                         </div>
                         <div className="box relative p-5 md:p-10 lg:p-20 flex items-center">
-                            <FontAwesomeIcon icon={faEnvelope}  size="2x"/>
+                        <FaEnvelope  className="icon text-3xl"/>
                             <div className="text ml-3 md:ml-5 text-black text-base flex-col font-light">
                                 <h3 className="font-medium text-green-600">Email</h3>
                                 <p>thevillauna@gmail.com</p>
@@ -50,12 +52,13 @@ const ContactUs: FC = () => {
                                 <textarea className="w-full py-1 px-0 text-base my-2 border-b-2 border-gray-700 focus:border-black outline-none resize-none" placeholder="Type your message here..." required></textarea>
                             </div>
                             <div className="inputBox relative mt-5 md:mt-10 w-full">
-                                <input className="w-full py-1 px-0 text-base my-2 border-b-2 border-gray-700 focus:border-black outline-none resize-none w-24 bg-green-600 text-white border-none cursor-pointer py-2 px-4 text-xs" type="submit" name="" value="Send" />
+                                <input className="w-full py-1 px-0 text-base my-2 border-b-2 border-gray-700 focus:border-black outline-none resize-none w-24 bg-green-600 text-white border-none cursor-pointer py-2 px-4 " type="submit" name="" value="Send" />
                             </div>
                         </form>
                     </div>
                 </div>
             </section>
+            
         </>
     )
 }
--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -5,12 +5,7 @@ import { BackToTop, Footer, NavigationWrapper } from "@/components";
 import { Toaster } from "@/components/ui/toaster";
 import { Suspense } from "react";
 import Loading from "./loading";
-// The following import prevents a Font Awesome icon server-side rendering bug,
-// where the icons flash from a very large icon down to a properly sized one:
-import '@fortawesome/fontawesome-svg-core/styles.css';
-// Prevent fontawesome from adding its CSS since we did it manually above:
-import { config } from '@fortawesome/fontawesome-svg-core';
-config.autoAddCss = false; /* eslint-disable import/first */
+
 
 const poppins = Poppins({
   subsets: ["latin"],
