387e68e93438b3d58178e3429d79c4dbb1c40803
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2024-05-08 00:33:24 +0530

    minor changes in hotel 3D model

--- src/components/custom/hotel/rooms-location.tsx
+++ src/components/custom/hotel/rooms-location.tsx
@@ -8,7 +8,7 @@ import { OrbitControls } from "@react-three/drei";
 export const RoomsLocation: FC = () => {
   return (
     <section className="h-screen">
-      <Canvas camera={{ fov: 35, zoom: 3, near: 1, far: 1000 }}>
+      <Canvas camera={{ fov: 35, zoom: 5, near: 1, far: 1000 }}>
         <color attach="background" args={["#101010"]} />
         <OrbitControls
           enableDamping={false}
@@ -16,6 +16,8 @@ export const RoomsLocation: FC = () => {
           enablePan={false}
           enableZoom
           rotation={[0.1, 0, 0]}
+          maxDistance={200}
+          minDistance={20}
         />
         <directionalLight position={[0, 3, 1]} intensity={1.2} />
         <ambientLight intensity={0.4} />
