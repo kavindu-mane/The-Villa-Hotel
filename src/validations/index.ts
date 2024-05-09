ef6c3b923a96a0ac5b8b8374eb5f4b17e199c79c
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2024-05-09 23:41:21 +0530

    add validations to restaurant forms

--- src/validations/index.ts
+++ src/validations/index.ts
@@ -132,3 +132,83 @@ export const ContactUsSchema = z.object({
     .min(1, { message: "Message field has to be filled." })
     .max(500, { message: "Message should contain maximum 500 characters." }),
 });
+
+// form schema for restaurant table availability validation
+export const RestaurantAvailabilitySchema = z.object({
+  date: z
+    .date()
+    .refine((date) => {
+      return !!date;
+    }, "Check-in date is required")
+    .refine((date) => {
+      const today = new Date();
+      return date >= today;
+    }, "Date must be greater than or equal to tomorrow")
+    .refine((date) => {
+      const lastDay = new Date();
+      // 1 months from now
+      lastDay.setMonth(lastDay.getMonth() + 1);
+      return date <= lastDay;
+    }, "Date must be less than or equal to 1 months from now"),
+  time_slot: z.string().min(1, {
+    message: "Please select a time slot",
+  }),
+  // .refine((value) => {
+  //   const timeSlots = [
+  //     "Morning (9:00 AM - 12:00 PM)",
+  //     "Afternoon (12:00 PM - 3:00 PM)",
+  //     "Evening (3:00 PM - 6:00 PM)",
+  //     "Night (6:00 PM - 9:00 PM)",
+  //   ];
+  //   if (value && !timeSlots.includes(value)) {
+  //     return {
+  //       message: "Please select a valid time slot",
+  //     };
+  //   }
+  // }),
+});
+
+// form schema for restaurant reservation validation
+export const RestaurantReservationSchema = z.object({
+  name: z
+    .string()
+    .min(1, { message: "Name field has to be filled." })
+    .max(50, { message: "Name should contain maximum 50 characters." }),
+  email: z
+    .string()
+    .min(1, { message: "Email field has to be filled." })
+    .email("This is not a valid email."),
+  phone: z
+    .string()
+    .min(1, { message: "Phone field has to be filled." })
+    .max(15, { message: "Phone should contain maximum 15 characters." }),
+  table: z.coerce
+    .number({ invalid_type_error: "Please select a table" })
+    .refine((val) => val !== undefined, {
+      message: "Table field has to be filled.",
+    })
+    .refine((val) => !isNaN(val), {
+      message: "Table must be a number",
+    })
+    .refine((val) => val >= 1 && val <= 20, {
+      message: "Table number must be between 1 and 20",
+    }),
+  remark: z
+    .string()
+    .max(500, { message: "Message should contain maximum 500 characters." })
+    .optional()
+    .or(z.literal("")),
+});
+
+// form schema for restaurant menu selection validation
+export const RestaurantMenuSchema = z.object({
+  menu: z.array(
+    z.object({
+      id: z.string(),
+      quantity: z
+        .number()
+        .int()
+        .min(1, { message: "Quantity must be at least 1" }),
+    }),
+  ),
+});
