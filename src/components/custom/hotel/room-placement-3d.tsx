48da8c92967677d369eb945ae55628c2488e26be
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2024-05-10 20:36:02 +0530

    room model implementation improve

--- src/components/custom/hotel/room-placement-3d.tsx
+++ src/components/custom/hotel/room-placement-3d.tsx
@@ -5,7 +5,7 @@ import { PresentationControls, Stage } from "@react-three/drei";
 
 export const RoomsPlacement = () => {
   return (
-    <PresentationControls speed={1.5} global zoom={0.2} polar={[0, 0]}>
+    <PresentationControls speed={1.5} polar={[0, 0]}>
       <Stage environment={"city"} intensity={0.3} shadows={false}>
         {/* models */}
         <HotelModel />
