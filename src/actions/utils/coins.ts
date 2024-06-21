857683af08b92c8a333579df491cbe8a4011f661
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2024-06-21 23:05:22 +0530

    add coins and connect user accounts to reservations

---
+++ src/actions/utils/coins.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { db } from "@/lib/db";
+
+// increase coins
+export const increaseCoins = async (userId: string, coins: number) => {
+  try {
+    const user = await db.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        coins: {
+          increment: coins,
+        },
+      },
+    });
+    return user;
+  } catch (e) {
+    return null;
+  }
+};
